9297db3240ecd5bd33f0c0e959250825b6571c82
- <punit.a@capgemini.com>
2025-06-02 17:54:59 +0530

    done

--- main.go
+++ main.go
@@ -4,66 +4,71 @@ import (
 	"fmt"
 )
 
-var stack []int
+type StackOfInt struct {
+	structInput []int
+}
 
-var anyDataTypeStack []interface{}
+type AnyDataTypeStack struct {
+	anyDataType []interface{}
+}
 
-func pushAnyData(value interface{}) {
-	anyDataTypeStack = append(anyDataTypeStack, value)
+func (d *AnyDataTypeStack) pushAnyData(value interface{}) {
+	d.anyDataType = append(d.anyDataType, value)
 	fmt.Println("Pushed (value):", value)
 }
 
-func popFromAnyDataTypeStack() {
-	if len(anyDataTypeStack) == 0 {
+func (d *AnyDataTypeStack) popFromAnyDataTypeStack() {
+	if len(d.anyDataType) == 0 {
 		fmt.Println("stack is empty. Cannot perform pop operation.")
 		return
 	}
-	lastElement := anyDataTypeStack[len(anyDataTypeStack)-1]
-	anyDataTypeStack = anyDataTypeStack[:len(anyDataTypeStack)-1]
+	lastElement := d.anyDataType[len(d.anyDataType)-1]
+	d.anyDataType = d.anyDataType[:len(d.anyDataType)-1]
 	fmt.Println("Popped Element:", lastElement)
 }
 
-func push(value int) {
-	stack = append(stack, value)
+func (data *StackOfInt) push(value int) {
+	data.structInput = append(data.structInput, value)
 	fmt.Println("Pushed : ", value)
 }
 
-func pop() {
+func (data *StackOfInt) pop() {
 
-	if len(stack) == 0 {
+	if len(data.structInput) == 0 {
 		fmt.Println("Stack is empty. Cannot perform pop operation.")
 		return
 	}
-	lastElemet := stack[len(stack)-1]
-	stack = stack[:len(stack)-1]
+	lastElemet := data.structInput[len(data.structInput)-1]
+	data.structInput = data.structInput[:len(data.structInput)-1]
 
 	fmt.Println("Poped Element : ", lastElemet)
 }
 
 func main() {
-	push(1)
-	push(2)
-	push(3)
-	fmt.Println(stack)
-	pop()
-	pop()
-	pop()
-	pop()
-	fmt.Println("After Pop Operation ", stack)
+	intStack := StackOfInt{}
+	intStack.push(1)
+	intStack.push(2)
+
+	fmt.Println(intStack.structInput)
+	intStack.pop()
+	intStack.pop()
+	intStack.pop()
+
+	fmt.Println("After Pop Operation ", intStack.structInput)
 
 	// AnyDataType Stack Operations
-	pushAnyData(42)
-	pushAnyData("GoLang")
-	pushAnyData(3.14)
-	pushAnyData(true)
-	pushAnyData([]int{1, 2, 3})
-	pushAnyData(map[string]int{"a": 1})
+	anyStack := AnyDataTypeStack{}
+	anyStack.pushAnyData(42)
+	anyStack.pushAnyData("GoLang")
+	anyStack.pushAnyData(3.14)
+	anyStack.pushAnyData([]int{1, 2, 3})
+	anyStack.pushAnyData(map[string]int{"a": 1})
 
-	fmt.Println("Any Data Type Stack:", anyDataTypeStack)
+	fmt.Println("Any Data Type Stack:", anyStack.anyDataType)
 
-	popFromAnyDataTypeStack()
-	popFromAnyDataTypeStack()
+	anyStack.popFromAnyDataTypeStack()
+	anyStack.popFromAnyDataTypeStack()
 
-	fmt.Println("After Pop Operation in AnyDataTypeStack : ",anyDataTypeStack)
+	fmt.Println("After Pop Operation in AnyDataTypeStack : ", anyStack.anyDataType)
 
 }
